ef1d82efcbb25f05349847d276863336336cc016
ttlhi10 <ttlhi10@gmail.com>
2021-09-22 16:35:36 +0900

    rust study - module - etc. pub,use ...

--- 5.Structure/rectangles/src/main.rs
+++ 5.Structure/rectangles/src/main.rs
@@ -20,15 +20,18 @@ fn main() {
     // );
 
     //structure
-    let rectangle_instance = Reactangle {length : 50, width : 30};
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area_structure(&rectangle_instance)
-    // );
+    let rectangle_instance = Rectangle {length : 50, width : 30};
+    
+    println!("{} ", rectangle_instance.area());
+    
+    let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    println!(
-        "{:?} test",  rectangle_instance
-    );
+    
 }
 
 // //function
@@ -37,11 +40,20 @@ fn main() {
 // }
 
 #[derive(Debug)]
-struct Reactangle{
+struct Rectangle{
     length : u32,
     width : u32,
 }
 
+impl Rectangle{
+    fn area(&self) ->u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other : &Rectangle) -> bool{
+        self.length > other.length && self.width > other.width
+    }
+}
 //structure
 // fn area_structure(rectangle : &Reactangle) -> u32{
 //     rectangle.length * rectangle.width
@@ -49,6 +61,6 @@ struct Reactangle{
 
 // //tuple
 // fn area_tuple(dimension : (u32, u32)) -> u32{
-//     dimension.0* dimension.1
-// }
+    //     dimension.0* dimension.1
+//
 
