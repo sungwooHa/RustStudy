9942f25b472c518bed875b2c15b56ab5c6875abb
hsw0312 <hsw0312@midasit.com>
2021-10-18 16:30:27 +0900

    chapter 8 quiz 3

--- 8.Collections/exam_3/src/lib.rs
+++ 8.Collections/exam_3/src/lib.rs
@@ -1,18 +1,72 @@
 pub mod exam{
     pub mod exam_3{
-        pub fn input_query() -> String{
+        use std::collections::HashMap;
+        use std::io;
 
-            use std::io;
+        pub enum Query{
+            ADD{department :  String, name : String},
+            LIST(String),
+            ALL,
+            QUIT,
+        }
+
+        impl Query
+        {
+            pub fn query_to_command(str_query : &Vec<String>) -> Option<Self>{
+                
+                let str_query : Vec<&str>  = str_query.iter().map(|s|s.as_ref()).collect();
+
+                match str_query.as_slice() {
+                    ["All"] => Some(Query::ALL),
+                    ["Quit"] => Some(Query::QUIT),
+                    ["List", department] => Some(Query::LIST(department.to_string())),
+                    ["Add", name, "to", department] => Some(Query::ADD{
+                        name :  name.to_string(),
+                        department : department.to_string()
+                    }),
+                    _ => None,
+                }
+            }
+        }
+
+        pub fn get_query() -> Vec<String>{
             println!("input query");
-
             let mut input = String::new();
             io::stdin().read_line(&mut input).expect("Failed to read query");
-
             let input = input.trim();
             if input.is_empty(){
                 panic!("invalid query(doesn't support space only)");
             }
-            input.to_string()
+            
+            let mut output_string:Vec<String> = Vec::new();
+            let words : Vec<&str> = input.split_whitespace().collect();
+            for word in words {
+                output_string.push(String::from(word));
+            }
+            output_string
+        }
+        
+        pub fn execute_add(department : &String, name : &String, employee : &mut HashMap<String, Vec<String>>){
+             employee.entry(department.to_string()).or_default().push(name.to_string());
+        }
+        pub fn execute_list(department : &String, employee : &HashMap<String, Vec<String>>){
+            match employee.get(department){
+                Some(names) => {
+                    for name in names {
+                        println!("{} : {}", department, name);
+                    }
+                },
+                None=> println!("can't find that department"),
+            }
+        }
+        pub fn execute_all(employee : &HashMap<String, Vec<String>>){
+            for (department, names) in employee {
+                let mut names = names.clone();
+                names.sort();
+                for name in names {
+                    println!("{} : {} ", department, name);
+                }
+            }
         }
     }
 }
--- 8.Collections/exam_3/src/main.rs
+++ 8.Collections/exam_3/src/main.rs
@@ -1,7 +1,25 @@
 mod lib;
 use lib::exam::exam_3;
+use std::collections::HashMap;
 
 fn main() {
-    println!("query : {} ", exam_3::input_query());
 
+    println!("Type 'Add <name> to <department>' to add an employee");
+    println!("Type 'List <department>' to list the employees of a department");
+    println!("Type 'All' to list all employees by department");
+    println!("Type 'Quit' to quit");
+
+    let mut employee : HashMap<String, Vec<String>> = HashMap::new();
+
+    loop{        
+        let query = exam_3::get_query();
+        match exam_3::Query::query_to_command(&query){
+            Some(exam_3::Query::ADD{department, name}) => exam_3::execute_add(&department, &name, &mut employee) ,
+            Some(exam_3::Query::LIST(department)) =>  exam_3::execute_list(&department, &employee),
+            Some(exam_3::Query::ALL) => exam_3::execute_all(&employee),
+            Some(exam_3::Query::QUIT) => break,
+
+            None => println!("Input Error"),
+        }
+    }
 }
