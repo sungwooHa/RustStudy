10dadbb490fb67fb7705f0260745e37983965102
hsw0312 <hsw0312@midasit.com>
2021-10-25 15:27:41 +0900

    10. generic example

--- 10.Generic trait lifetime/trait_example/src/lib.rs
+++ 10.Generic trait lifetime/trait_example/src/lib.rs
@@ -9,7 +9,7 @@ pub fn notify<T: Summarizable>(item : T){
     println!("Breaking  news! {}", item.summary());
 }
 
-pub fn largest<T : PartialOrd + Copy>(list: &[T]) -> T{
+pub fn largest_1<T : PartialOrd + Copy>(list: &[T]) -> T{
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -19,6 +19,18 @@ pub fn largest<T : PartialOrd + Copy>(list: &[T]) -> T{
 
     largest
 }
+
+pub fn largest_2<T : PartialOrd>(list: &[T]) -> &T{
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 
 
 pub struct NewsArticle{
--- 10.Generic trait lifetime/trait_example/src/main.rs
+++ 10.Generic trait lifetime/trait_example/src/main.rs
@@ -1,7 +1,8 @@
 mod lib;
 
 use lib::Summarizable;
-use lib::largest;
+use lib::largest_1;
+use lib::largest_2;
 use lib::Tweet;
 
 fn main() {
@@ -17,11 +18,11 @@ fn main() {
     println!("1 new Tweet : {}", tweet.author_summary());
 
     let numbers = vec![34,50,25,100,63];
-    let result = largest(&numbers);
+    let result = largest_1(&numbers);
     println!("the largest number is {}", result);
 
     let chars = vec!['y', 'm', 'a', 'q'];
-    let result = largest(&chars);
+    let result = largest_2(&chars);
     println!("The largest char is {}", result);
 }
 
