435236c69b50a038839a54ed4c1d9fb0760436a9
hsw0312 <hsw0312@midasit.com>
2021-10-25 11:15:08 +0900

    generic structure implement

--- 10.Generic trait lifetime/generic/src/main.rs
+++ 10.Generic trait lifetime/generic/src/main.rs
@@ -1,7 +1,17 @@
 
-struct Point<T, U>{
+struct Point<T,U>{
     x : T,
-    y : U,
+    y: U,
+}
+
+impl<T, U> Point<T, U>{
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
 }
 
 fn main() {
@@ -9,7 +19,7 @@ fn main() {
     let both_float = Point { x: 1.0, y: 4.0 };
     let integer_and_float = Point { x: 5, y: 4.0 };
 
-    println!("x {}, y {}", both_integer.x, both_integer.y);
+    println!("x {}, y {}", both_integer.x(), both_integer.y);
     println!("x {}, y {}", both_float.x, both_float.y);
     println!("x {}, y {}", integer_and_float.x, integer_and_float.y);
 }
