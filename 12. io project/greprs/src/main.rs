5c202f85a7b6bf8052648112ff7e5d96ebef972a
hsw0312 <hsw0312@midasit.com>
2021-11-01 14:35:21 +0900

    lib rs 분리

--- 12. io project/greprs/src/main.rs
+++ 12. io project/greprs/src/main.rs
@@ -1,36 +1,8 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
-
-struct Config {
-    query : String,
-    filename : String,
-}
-
-impl Config{
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
-}
-
-fn run(config : Config) -> Result<(), Box<dyn Error>>{
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;//expect("SomeThing went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
+use greprs::Config;
 
 
 fn main() {
@@ -43,7 +15,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = greprs::run(config) {
         println!("Application error : {}", e);
         process::exit(1);
     }
