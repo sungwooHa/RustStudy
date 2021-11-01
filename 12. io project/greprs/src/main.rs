d244063686cbcb6e709487b39d1ab1f205bff344
hsw0312 <hsw0312@midasit.com>
2021-11-01 14:19:16 +0900

    Config Result 적용해보기

--- 12. io project/greprs/src/main.rs
+++ 12. io project/greprs/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 struct Config {
     query : String,
@@ -8,21 +9,23 @@ struct Config {
 }
 
 impl Config{
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
-        
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments : {} ", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
