74000101f227cbcbfeecdaec012368707e2b21e4
sungwooHa <ttlhi10@gmail.com>
2021-11-08 17:48:11 +0900

    11 io program refactoring

--- 12. io project/greprs/src/lib.rs
+++ 12. io project/greprs/src/lib.rs
@@ -10,12 +10,20 @@ pub struct Config {
 }
 
 impl Config{
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-        return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args : std::env::Args) -> Result<Config, &'static str> {
+        //first args is program name
+        args.next();
+
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {query, filename, case_sensitive})
@@ -77,6 +85,17 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
+
+    /*
+    //filter를 이용해 반복자에 조건을 걸 수 있고, 조건에 걸린 iter를 collect를 통해 vec로 받는다
+    
+    //other way
+
     let mut results = Vec::new();
 
     for line in contents.lines(){
@@ -85,7 +104,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
     results
+
+    */
 }
+
 
 pub fn search_case_insensitive<'a>(query : &str, contents : &'a str) -> Vec<&'a str>{
     let query = query.to_lowercase();
