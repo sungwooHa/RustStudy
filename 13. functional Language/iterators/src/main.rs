06e40244ce998d76c4c1bd855176c972a5d8bd15
sungwooHa <ttlhi10@gmail.com>
2021-11-08 17:39:14 +0900

    13.2 iterator end

--- 13. functional Language/iterators/src/main.rs
+++ 13. functional Language/iterators/src/main.rs
@@ -1,75 +1,16 @@
+extern crate iterators;
+
+use iterators::Counter;
+
 fn main() {
     println!("Hello, world!");
     
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    for val in v1_iter {
-        println!("got : {}", val);
-    }
-}
+    let sum : Vec<_> = Counter::new()
+                            .zip(Counter::new().skip(1)) //1은 스킵.
+                            .map(|(a,b)| a * b)
+                            .filter(|x| x % 3 == 0)
+                            .collect()
+                            ;
 
-
-#[test]
-fn iterator_demonstration(){
-    let v1 = vec![1,2,3];
-    let mut v1_iter = v1.iter();
-
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
-}
-
-#[derive(PartialEq, Debug)]
-struct Shoe{
-    size : u32,
-    style : String,
-}
-
-fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size:u32) -> Vec<Shoe>{
-    shoes.into_iter()
-        .filter(|s| s.size == shoe_size)
-        .collect()
-}
-
-#[test]
-fn filters_by_size(){
-    let shoes = vec![
-        Shoe{size : 10, style : String::from("sneaker")},
-        Shoe{size : 13, style : String::from("sandal")},
-        Shoe{size : 10, style : String::from("boot")},
-    ];
-
-    let in_my_size = shoes_in_my_size(shoes, 10);
-
-    assert_eq!(
-        in_my_size,
-        vec![
-            Shoe{size : 10, style : String::from("sneaker")},
-            Shoe{size : 10, style : String::from("boot")},
-        ]
-    );
-}
-
-struct Counter{
-    count : u32,
-}
-
-impl Counter{
-    fn new() -> Counter{
-        Counter {count : 0}
-    }
-}
-
-impl Iterator for Counter{
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item>{
-        self.count += 1;
-
-        if self.count < 6{
-            Some(self.count)
-        }else{
-            None
-        }
-    }
+    println!("{:?}", sum);
 }
