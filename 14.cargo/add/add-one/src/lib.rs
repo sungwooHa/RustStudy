d1127fb6c47542263301a3065fc5608ae7edaaa6
ttlhi10 <ttlhi10@gmail.com>
2021-11-15 16:15:13 +0900

    Box_t

--- 14.cargo/add/add-one/src/lib.rs
+++ 14.cargo/add/add-one/src/lib.rs
@@ -1,6 +1,30 @@
+
+/// adds one to the number given.
+/// # Example1
+/// ### Example 1-1
+/// ### Example 1-2
+/// ```
+/// assert_eq!(1, my_create::add_one(0));
+/// assert_eq!(2, my_create::add_one(1));
+/// ```
+/// ```
+/// code text
+/// ```
+/// normal text
 pub fn add_one(x : i32) -> i32{
     x+1
 }
+
+/// adds two to the number given.
+/// # Example
+/// ```
+/// assert_eq!(2, my_create::add_one(0));
+/// assert_eq!(3, my_create::add_one(1));
+/// ```
+pub fn add_two(x : i32) -> i32{
+    x+2
+}
+
 
 #[cfg(test)]
 mod tests {
