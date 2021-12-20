ce7106f6fef696bc0cdf05c4ca981110dd73039d
hsw0312 <hsw0312@midasit.com>
2021-12-20 17:03:39 +0900

    tree data structure visualization

--- 15.smart/tree_data_structure/src/main.rs
+++ 15.smart/tree_data_structure/src/main.rs
@@ -8,7 +8,8 @@ struct Node {
     parent: RefCell<Weak<Node>>,
 }
 
-fn main() {
+#[allow(dead_code)]
+fn make_tree(){
     let leaf = Rc::new(Node {
         value: 3,
         children: RefCell::new(vec![]),
@@ -24,4 +25,52 @@ fn main() {
 
     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
     println!("leaf parent = {:?}", leaf.parent.borrow_mut().upgrade());
+}
+
+fn make_tree_visualization(){
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
+
+fn main() {
+    make_tree_visualization();
 }
