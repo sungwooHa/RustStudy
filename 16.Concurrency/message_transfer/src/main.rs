10fb1eb710cc83b04316bb78dd5774c421f9922e
sungwooHa <ttlhi10@gmail.com>
2021-12-28 01:08:40 +0900

    ch16 multi transmitter

--- 16.Concurrency/message_transfer/src/main.rs
+++ 16.Concurrency/message_transfer/src/main.rs
@@ -1,7 +1,8 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
-fn main() {
+fn channel_test(){
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move ||{
@@ -11,4 +12,64 @@ fn main() {
 
     let received = rx.recv().unwrap();
     println!("Got : {}", received);
+}
+
+fn channel_multi_rx(){
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn main() {
+    //  channel_multi_rx();
+        
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
