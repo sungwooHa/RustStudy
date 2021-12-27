547f2ecefe6277c8cf9aa0aa3a8b6363e5988985
sungwooHa <ttlhi10@gmail.com>
2021-12-27 16:38:19 +0900

    test code

--- 3.Common programming concept/loops/src/main.rs
+++ 3.Common programming concept/loops/src/main.rs
@@ -1,20 +1,8 @@
-
-fn plus_one(x: i32) -> i32 
-{
-    x + 1
-}
-
-
-fn main() 
-{
-    let mut tmp_number = 1;
-
-    while tmp_number != 10
-    {
-        println!("result of function : {}", plus_one(tmp_number));
-        tmp_number += 1;
+#![allow(unused)]
+fn main() {
+'outer: loop {
+    while true {
+        break 'outer;
     }
-
-
-    println!("Hello, world!");
+}
 }
