1af194052efd71ca292258dbec84082de2d349fb
sungwooHa <ttlhi10@gmail.com>
2021-12-27 16:38:34 +0900

    test for study

--- 5.Structure/rectangles/src/main.rs
+++ 5.Structure/rectangles/src/main.rs
@@ -1,66 +1,99 @@
 
 
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
 
 fn main() {
-    
-    // let length : u32 = 50;
-    // let width : u32 = 30;
+    let my_string = String::from("hello world");
 
+    let word = first_word(&my_string);
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
 
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area(length, width)
-    // );
+    let my_string_literal = "hello world";
 
-    // //tuple
-    // let rectangle_measurement = (50,30);
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area_tuple(rectangle_measurement)
-    // );
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
 
-    //structure
-    let rectangle_instance = Rectangle {length : 50, width : 30};
-    
-    println!("{} ", rectangle_instance.area());
-    
-    let rect1 = Rectangle { length: 50, width: 30 };
-    let rect2 = Rectangle { length: 40, width: 10 };
-    let rect3 = Rectangle { length: 45, width: 60 };
-    
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-
-    
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 }
 
-// //function
-// fn area(length : u32, width : u32) -> u32 {
-//     length*width
+// fn main() {
+    
+//     // let length : u32 = 50;
+//     // let width : u32 = 30;
+
+
+//     // println!(
+//     //     "The area of the rectangle is {} square pixels.",
+//     //     area(length, width)
+//     // );
+
+//     // //tuple
+//     // let rectangle_measurement = (50,30);
+//     // println!(
+//     //     "The area of the rectangle is {} square pixels.",
+//     //     area_tuple(rectangle_measurement)
+//     // );
+
+//     //structure
+//     let rectangle_instance = Rectangle {length : 50, width : 30};
+    
+//     println!("{} ", rectangle_instance.area());
+    
+//     let rect1 = Rectangle { length: 50, width: 30 };
+//     let rect2 = Rectangle { length: 40, width: 10 };
+//     let rect3 = Rectangle { length: 45, width: 60 };
+    
+//     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+//     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    
 // }
 
-#[derive(Debug)]
-struct Rectangle{
-    length : u32,
-    width : u32,
-}
+// // //function
+// // fn area(length : u32, width : u32) -> u32 {
+// //     length*width
+// // }
 
-impl Rectangle{
-    fn area(&self) ->u32 {
-        self.length * self.width
-    }
-
-    fn can_hold(&self, other : &Rectangle) -> bool{
-        self.length > other.length && self.width > other.width
-    }
-}
-//structure
-// fn area_structure(rectangle : &Reactangle) -> u32{
-//     rectangle.length * rectangle.width
+// #[derive(Debug)]
+// struct Rectangle{
+//     length : u32,
+//     width : u32,
 // }
 
-// //tuple
-// fn area_tuple(dimension : (u32, u32)) -> u32{
-    //     dimension.0* dimension.1
-//
+// impl Rectangle{
+//     fn area(&self) ->u32 {
+//         self.length * self.width
+//     }
+
+//     fn can_hold(&self, other : &Rectangle) -> bool{
+//         self.length > other.length && self.width > other.width
+//     }
+// }
+// //structure
+// // fn area_structure(rectangle : &Reactangle) -> u32{
+// //     rectangle.length * rectangle.width
+// // }
+
+// // //tuple
+// // fn area_tuple(dimension : (u32, u32)) -> u32{
+//     //     dimension.0* dimension.1
+// //
 
