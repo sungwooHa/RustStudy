8d63550001c4fd395715ec9cff2c0132432b407d
MIDASIT\hsw0312 <hsw0312@midasit.com>
2022-02-14 18:57:35 +0900

    17 oop rust

---
+++ 17.oop rust/oop_state_pattern/src/lib.rs
@@ -0,0 +1,27 @@
+
+trait State{
+
+}
+
+struct Draft{
+
+}
+
+impl State for Draft{
+    
+}
+
+pub struct Post{
+    state : Option<Box<State>>,
+    content : String,
+}
+
+impl Post {
+    pub fn new() -> Post{
+        Post{
+            state:Some(Box::new(Draft {}));
+            content : String::new(),
+        }
+    }
+    
+}
