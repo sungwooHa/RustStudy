acee154ddf200747ecf8d3b498e26d0840640c2d
MIDASIT\hsw0312 <hsw0312@midasit.com>
2022-02-14 16:48:05 +0900

    difference between generic and trait bound

--- 17.oop rust/draw_trait/src/lib.rs
+++ 17.oop rust/draw_trait/src/lib.rs
@@ -1,22 +1,30 @@
 pub trait Draw{
     fn draw(&self);
 }
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
 
-pub struct Pencil {}
-pub struct Ballpen {}
-
-impl Draw for Pencil{
+impl Draw for Button {
     fn draw(&self) {
-        println!("pencil draw");
+        println!("Button Draw");
+    }
+}
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("SelectBox Draw");
     }
 }
 
-impl Draw for Ballpen{
-    fn draw(&self){
-        println!("ballpen draw");
-    }
-}
-
+//trait bound
 pub struct Screen{
     pub components : Vec<Box<dyn Draw>>,
 }
@@ -28,3 +36,17 @@ impl Screen {
         }
     }
 }
+
+//Generic type parameter
+pub struct Screen_generic<T: Draw> {
+    pub components : Vec<T>,
+}
+
+impl<T> Screen_generic<T> where T: Draw{
+    pub fn run(&self) {
+        for component in self.components.iter(){
+            component.draw();
+        }
+    }
+}
+
--- 17.oop rust/draw_trait/src/main.rs
+++ 17.oop rust/draw_trait/src/main.rs
@@ -1,14 +1,62 @@
-use draw_trait::{Screen, Pencil, Ballpen};
+use draw_trait::{Screen, SelectBox, Button, Screen_generic};
 
 fn main() {
     println!("Hello, world!");
 
     let screen = Screen {
         components : vec![
-            Box::new(Pencil {}),
-            Box::new(Ballpen {}),
+            Box::new(SelectBox {
+                width : 75,
+                height : 10,
+                options : vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width : 75,
+                height : 10,
+                label : String::from("Ok"),
+            }),
         ],
     };
 
     screen.run();
+
+
+    let screen_generic = Screen_generic{
+        components : vec![
+            SelectBox{
+                width : 75,
+                height : 10,
+                options : vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            },
+            SelectBox{
+                width : 75,
+                height : 10,
+                options : vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            },
+            SelectBox{
+                width : 75,
+                height : 10,
+                options : vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            },
+        ],
+    };
+
+    screen_generic.run();
+
 }
